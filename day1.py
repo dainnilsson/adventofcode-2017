3b70dda08209e3115ee8b354bef23adc6aabdf3d
Dain Nilsson <dain@yubico.com>
2017-12-01 10:25:17 +0100

    Use enumerate.

--- day1.py
+++ day1.py
@@ -5,5 +5,5 @@ data = input().strip()
 ds = [int(d) for d in data]
 n = len(ds)
 
-print(sum(ds[i] for i in range(n) if ds[i] == ds[(i+1) % n]))
-print(sum(ds[i] for i in range(n) if ds[i] == ds[(i+n//2) % n]))
+print(sum(d for i, d in enumerate(ds) if d == ds[(i+1) % n]))
+print(sum(d for i, d in enumerate(ds) if d == ds[(i+n//2) % n]))
