c0047a2e4fba69e0fa7fde3a656f6d0615ce9c62
Dain Nilsson <dain@yubico.com>
2017-12-07 13:18:36 +0100

    Use index to simplify.

--- day6.py
+++ day6.py
@@ -6,8 +6,9 @@ for _ in [0]*2:
     s = set()
     while tuple(d) not in s:
         s.add(tuple(d))
-        n, i = max((n, -i) for (i, n) in enumerate(d))
-        d[-i] = 0
+        n = max(d)
+        i = d.index(n)
+        d[i] = 0
         for j in range(n):
-            d[(j-i+1) % len(d)] += 1
+            d[(i+j+1) % len(d)] += 1
     print(len(s))
