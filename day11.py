79f5f85a8aceed5d9fe18dde3d8b5fec8cb152f9
Dain Nilsson <dain@yubico.com>
2017-12-11 10:01:35 +0100

    Day 11.

---
+++ day11.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+data = input().strip().split(',')
+
+d = lambda x, y: abs(x)//2+abs(y)
+x = y = m = 0
+for s in data:
+    y += 1 if s == 'n' or x % 2 and 'n' in s else -1 if s == 's' or not x % 2 and 's' in s else 0
+    x += -1 if 'w' in s else 1 if 'e' in s else 0
+    m = max(m, d(x, y))
+print(d(x, y))
+print(m)
