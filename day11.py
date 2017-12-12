309182850a07f5279009dcee7d692053dd89a1e0
Dain Nilsson <dain@yubico.com>
2017-12-12 10:02:33 +0100

    Remove debug print.

--- day11.py
+++ day11.py
@@ -8,5 +8,5 @@ for s in data:
     y += 1 if s == 'n' or x % 2 and 'n' in s else -1 if s == 's' or not x % 2 and 's' in s else 0
     x += -1 if 'w' in s else 1 if 'e' in s else 0
     m = max(m, d(x, y))
-print(d(x, y), x, y)
+print(d(x, y))
 print(m)
