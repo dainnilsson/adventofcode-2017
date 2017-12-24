b323ca64dab9567ee894451d9c600e83291309f3
Dain Nilsson <dain@yubico.com>
2017-12-24 10:34:49 +0100

    Speed up day 23.

--- day24.py
+++ day24.py
@@ -5,16 +5,19 @@ import sys
 d = [tuple(int(y) for y in x.strip().split('/')) for x in sys.stdin.readlines()]
 
 
-def build(start, rem):
-    yield []
+def build(start, rem, maxlen=0, score=0):
+    best = []
     for left, right in rem:
         if start in (left, right):
             subrem = rem[:]
             subrem.remove((left, right))
+            myscore = score + left + right
             x = right if left == start else left
-            for rest in build(x, subrem):
-                yield [(left, right)] + rest
+            cand = [(myscore, left, right)] + build(x, subrem, maxlen, myscore)
+            best = cand if not best or (maxlen and len(cand) > len(best)) \
+                or cand[-1] > best[-1] else best
+    return best
 
 
-print(max(sum(l+r for (l, r) in b) for b in build(0, d)))
-print(max((len(b), sum(l+r for (l, r) in b)) for b in build(0, d))[1])
+print(build(0, d)[-1][0])
+print(build(0, d, 1)[-1][0])
